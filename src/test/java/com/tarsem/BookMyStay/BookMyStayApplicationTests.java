0c08944674c9074108c6b16eb94b263f0758834f
gittarsem <work4tarsemgulab@gmail.com>
2026-03-15 23:21:09 +0530

    Intialise the Project

---
+++ src/test/java/com/tarsem/BookMyStay/BookMyStayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarsem.BookMyStay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyStayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
