e577173aa36bb5d0fcdfeb2f237c280942c8e9d6
gittarsem <work4tarsemgulab@gmail.com>
2026-04-02 23:38:00 +0530

    feat: implement hotel search

--- src/main/java/com/tarsem/BookMyStay/Controller/HotelBrowseController.java
+++ src/main/java/com/tarsem/BookMyStay/Controller/HotelBrowseController.java
@@ -2,17 +2,23 @@ package com.tarsem.BookMyStay.Controller;
 
 import com.tarsem.BookMyStay.Service.Interfaces.HotelService;
 import com.tarsem.BookMyStay.dto.HotelInfoDTO;
+import com.tarsem.BookMyStay.dto.HotelPriceDTO;
+import com.tarsem.BookMyStay.dto.HotelSearchRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.Caching;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/hotels")
+@SecurityRequirement(name = "BearerAuth")
+@Tag(name = "Hotel Browse", description = "Browse and search for hotels")
 public class HotelBrowseController {
 
     @Autowired
@@ -21,6 +27,13 @@ public class HotelBrowseController {
     @GetMapping("/{hotelId}/info")
     public ResponseEntity<HotelInfoDTO> getHotelInfo(@PathVariable Long hotelId){
         return ResponseEntity.ok(hotelService.findHotelById(hotelId));
+    }
+
+    @GetMapping("/search")
+    @Operation(summary = "Search for hotels", description = "Filter hotels based on location, price, availability, etc.")
+    public ResponseEntity<Page<HotelPriceDTO>> searchHotels(@RequestBody HotelSearchRequest hotelSearchRequest) {
+        Page<HotelPriceDTO> page = inventoryService.searchHotels(hotelSearchRequest);
+        return ResponseEntity.ok(page);
     }
 
 }
