f65c7107f60f4ee24f441d888eff64e53acb8750
gittarsem <work4tarsemgulab@gmail.com>
2026-07-26 16:35:01 +0530

    feat(kafka): add type-safe Kafka producer service

---
+++ bookmystay-core/src/main/java/com/tarsem/BookMyStay/producer/KafkaProducerService.java
@@ -0,0 +1,19 @@
+package com.tarsem.BookMyStay.producer;
+
+import com.tarsem.BookMyStay.constants.KafkaTopics;
+import com.tarsem.bookmystay.events.booking.BookingConfirmedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@Slf4j
+@RequiredArgsConstructor
+public class KafkaProducerService {
+    private final KafkaTemplate<String, BookingConfirmedEvent> template;
+
+    public void publishConfirmedBooking(BookingConfirmedEvent event){
+        log.info("Publishing BookingConfirmedEvent for bookingId={}", event.getBookingId());
+        template.send(KafkaTopics.BOOKING_CONFIRMED,event);
+
+    }
+}
