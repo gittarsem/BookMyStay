fdb5c0f9afff5eda89e5804ae98be2af21cf2544
gittarsem <work4tarsemgulab@gmail.com>
2026-09-19 11:55:53 +0530

    fixed:JwtFilter.java for health endpoint

--- bookmystay-core/src/main/java/com/tarsem/BookMyStay/Config/JwtFilter.java
+++ bookmystay-core/src/main/java/com/tarsem/BookMyStay/Config/JwtFilter.java
@@ -35,6 +35,15 @@ public class JwtFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String path = request.getRequestURI();
+
+        // Public health endpoint
+        if ("/health".equals(path)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         String authHeader=request.getHeader("Authorization");
         String token=null;
         String username=null;
